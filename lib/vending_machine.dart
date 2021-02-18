cee37739c59cb9206f1bc4de978782ee149fe3b6
YeEun Kim <yeeun.kim.yu@gmail.com>
2021-02-18 17:41:39 +0900

    made front page
    and navigation to VM page

--- lib/vending_machine.dart
+++ lib/vending_machine.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'dart:async' show Future;
+import 'dart:convert';
 
 class VendingMachineMenu extends StatefulWidget {
   @override
@@ -6,10 +8,49 @@ class VendingMachineMenu extends StatefulWidget {
 }
 
 class _VendingMachineMenuState extends State<VendingMachineMenu> {
+  
+  int cash = 10000;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Virtual Vending Machine'),
+        actions: [
+          IconButton(
+              icon: Icon(Icons.shopping_cart_outlined),
+              onPressed: null
+          )
+        ]
+      ),
+      body: Column(
+        children: [
+          Text('제품 목록 들어갈 자리~~'),
+          Row(
+            children : <Widget>[
+              Text('남은 돈: '),
+              Text(cash.toString()),
+              IconButton(
+                  icon: Icon(Icons.indeterminate_check_box_rounded),
+                  onPressed: (){
+                    _pay(100);
+                  }
+              )
+            ]
+          )
+        ],
+      )
+    );
   }
+
+
+  void _pay(int price) {
+    setState(() {
+      cash -= price;
+    });
+  }
+
+
 }
 
 
