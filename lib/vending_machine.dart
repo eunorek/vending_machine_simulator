87771bb6ccfb44e380ff99704570b7a097ab574d
YeEun Kim <yeeun.kim.yu@gmail.com>
2021-02-18 14:42:39 +0900

    fixed drawer

--- lib/vending_machine.dart
+++ lib/vending_machine.dart
@@ -1,2 +1,15 @@
 import 'package:flutter/material.dart';
 
+class VendingMachineMenu extends StatefulWidget {
+  @override
+  _VendingMachineMenuState createState() => _VendingMachineMenuState();
+}
+
+class _VendingMachineMenuState extends State<VendingMachineMenu> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+
