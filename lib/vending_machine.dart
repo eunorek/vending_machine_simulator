0418651195a6f28c2e246cfd9834d2427db34a80
YeEun Kim <yeeun.kim.yu@gmail.com>
2021-02-18 18:31:50 +0900

    tried to parse json data
    and failed

--- lib/vending_machine.dart
+++ lib/vending_machine.dart
@@ -2,14 +2,29 @@ import 'package:flutter/material.dart';
 import 'dart:async' show Future;
 import 'dart:convert';
 
+import 'package:flutter/services.dart';
+
 class VendingMachineMenu extends StatefulWidget {
   @override
   _VendingMachineMenuState createState() => _VendingMachineMenuState();
 }
 
 class _VendingMachineMenuState extends State<VendingMachineMenu> {
-  
+  List productList;
   int cash = 10000;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    this.loadJsonData();
+  }
+
+  Future<String> loadJsonData() async {
+    var jsonText = await rootBundle.loadString('assets/data/products_data.json');
+    setState(() => productList = json.decode(jsonText));
+    return 'json data parsed';
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -35,7 +50,27 @@ class _VendingMachineMenuState extends State<VendingMachineMenu> {
                   onPressed: (){
                     _pay(100);
                   }
+              ),
+              ListView.builder(
+                  itemCount: productList != null ? productList.length : 0,
+                  itemBuilder: (BuildContext context, int index){
+                    var id = productList[index]['id'];
+                    var name = productList[index]['name'];
+                    var imgName = productList[index]['imgName'];
+                    var price = productList[index]['price'];
+                    var stock = productList[index]['stock'];
+
+                    return new Column(
+                      children: <Widget>[
+                        new ListTile(
+                          title: Text('name'),
+                        ),
+                        new Divider(),
+                      ],
+                    );
+                  }
               )
+
             ]
           )
         ],
@@ -50,6 +85,11 @@ class _VendingMachineMenuState extends State<VendingMachineMenu> {
     });
   }
 
+  GridView getProductGrid() {
+    return GridView.count(
+        crossAxisCount: 3,
+    );
+  }
 
 }
 
